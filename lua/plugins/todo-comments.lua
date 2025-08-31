4768d273a54567835e6697831330cecd4ec4ac0c
Diogo-V <diogo.venancio00@gmail.com>
2025-08-31 23:05:12 +0200

    chore: improve todos with jumps

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,25 @@ return {
   "folke/todo-comments.nvim",
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = { signs = false },
+  opts = {
+    signs = false,
+  },
+  keys = {
+    {
+      "<leader>ctN",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Previous todo comment",
+      mode = { "n" },
+    },
+    {
+      "<leader>ctn",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next todo comment",
+      mode = { "n" },
+    },
+  },
 }
