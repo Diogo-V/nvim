9e48d1b2196870cb302ce47b386ecb54ebbf49b4
Diogo-V <diogo.venancio00@gmail.com>
2025-08-31 17:27:35 +0200

    feat: add snacks dashboard and bigfile support

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
