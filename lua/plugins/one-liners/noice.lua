c30a6642afb2d9faf823090559af81ec70c417d5
Diogo-V <diogo.venancio00@gmail.com>
2025-09-09 08:25:56 +0100

    chore: update noice to center the cmd bar in the screen

--- lua/plugins/one-liners/noice.lua
+++ lua/plugins/one-liners/noice.lua
@@ -12,7 +12,21 @@ return { -- Modifies the cmd bar to be in the middle of the screen
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
+      command_palette = {
+        views = {
+          cmdline_popup = {
+            position = {
+              row = "35%",
+              col = "50%",
+            },
+            size = {
+              min_width = 60,
+              width = "auto",
+              height = "auto",
+            },
+          },
+        },
+      },
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
