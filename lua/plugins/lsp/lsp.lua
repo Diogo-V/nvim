261d5e012d358b6ecaab5c08d851463ed1e7a573
Diogo-V <diogo.venancio00@gmail.com>
2025-09-11 12:15:57 +0100

    chore: configure lua-ls

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -171,16 +171,23 @@ return { -- Handles LSP configuration
       bashls = {},
       ts_ls = {},
       -- rust_analyzer = {}, -- Rust will be configured through the language plugin "rustaceanvim"
-
       lua_ls = {
         settings = {
           Lua = {
             completion = {
               callSnippet = "Replace",
             },
+            runtime = { version = "LuaJIT" },
+            workspace = {
+              checkThirdParty = false,
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
             diagnostics = {
               globals = { "vim" },
               disable = { "missing-fields" },
+            },
+            format = {
+              enable = false,
             },
           },
         },
